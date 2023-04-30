f153a60c1994c5e7c10a2947bd1907b288d7b074
Matthew Rogers <rogersmatt350@gmail.com>
2023-04-29 19:13:20 -0500

    feat: init mantine ui

--- website/src/pages/_app.tsx
+++ website/src/pages/_app.tsx
@@ -1,6 +1,62 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { AppProps } from "next/app";
+import Head from "next/head";
+import {
+  ColorScheme,
+  ColorSchemeProvider,
+  MantineProvider,
+  useMantineTheme,
+} from "@mantine/core";
+import { Roboto, RobotoMono } from "@/lib/CustomFonts";
+import { useState } from "react";
+import { getCookie, setCookie } from "cookies-next";
+import { useHotkeys } from "@mantine/hooks";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App(props: AppProps) {
+  const { Component, pageProps } = props;
+  const theme = useMantineTheme();
+  const [colorScheme, setColorScheme] = useState<ColorScheme>("dark");
+
+  const toggleColorScheme = (value?: ColorScheme) => {
+    const nextColorScheme =
+      value || (colorScheme === "dark" ? "light" : "dark");
+    setColorScheme(nextColorScheme);
+    setCookie("mantine-color-scheme", nextColorScheme, {
+      maxAge: 60 * 60 * 24 * 30,
+    });
+  };
+
+  useHotkeys([["mod+J", () => toggleColorScheme()]]);
+
+  return (
+    <>
+      <Head>
+        <title>United Nations</title>
+        <link rel="shortcut icon" href="/static/images/favicon.ico" />
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+      <ColorSchemeProvider
+        colorScheme={colorScheme}
+        toggleColorScheme={toggleColorScheme}
+      >
+        <MantineProvider
+          withGlobalStyles
+          withNormalizeCSS
+          theme={{
+            primaryShade: 4,
+            fontFamily: `${Roboto.style.fontFamily}`,
+            colorScheme: colorScheme,
+            fontFamilyMonospace: `${RobotoMono.style.fontFamily}`,
+            headings: {
+              fontFamily: `${Roboto.style.fontFamily}`,
+            },
+          }}
+        >
+          <Component {...pageProps} />
+        </MantineProvider>
+      </ColorSchemeProvider>
+    </>
+  );
 }
