20402200387f8cba385b6002cccfbc359adb8f69
Matthew Rogers <rogersmatt350@gmail.com>
2023-05-06 13:24:01 -0500

    config pfp webp support

--- website/next.config.js
+++ website/next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ["source.boringavatars.com"],
+    formats: ["image/webp"],
+
+  }
 }
 
 module.exports = nextConfig
