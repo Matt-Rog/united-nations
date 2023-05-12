8250427fc793f05d5568e1b88bd74b27209666d9
Matthew Rogers <rogersmatt350@gmail.com>
2023-05-11 22:26:34 -0500

    add param support for un call

--- website/src/pages/api/un/index.ts
+++ website/src/pages/api/un/index.ts
@@ -10,7 +10,22 @@ export default async function handler(
   const method = body.method;
   const endpoint = body.endpoint;
   const payload = body.payload;
-  let url = base_url + endpoint + `?token=${token}`;
+  const params = body.params;
+  let url =
+    base_url +
+    endpoint +
+    `?${
+      params
+        ? params
+            .map((p: any) => {
+              var key = Object.keys(p)[0];
+              var val = Object.values(p)[0];
+              return `${key}=${val}`;
+            })
+            .join("&")
+        : ""
+    }&token=${token}`;
+
   let response = undefined;
   if (method === "GET") {
     response = await fetch(url, {
