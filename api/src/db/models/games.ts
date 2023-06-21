02a68fe803d18b60ee03dbfde300dcdbf74806e4
Matthew Rogers <rogersmatt350@gmail.com>
2023-06-20 20:55:06 -0500

    Revert "Add read service"
    This reverts commit 20b7fac93c789a3f86ebabfe1e26be853e1b9321.

--- api/src/db/models/games.ts
+++ api/src/db/models/games.ts
@@ -3,32 +3,12 @@ import { getSession } from "../conn";
 export interface GameSchema {
   id: string; // randomUUID()
   name: string;
-  guildId: string;
   createdAt: string;
 }
+
 function deserialize(g: GameSchema) {
   return g;
 }
-
-// READ
-export const getGameByProp = async (prop: any) => {
-  let key = Object.keys(prop)[0];
-  let db = await getSession();
-  const result = await db.run(
-    `MATCH (g:Game {
-      ${key}: $value
-    })
-    RETURN g`,
-    {
-      value: prop[key],
-    }
-  );
-  await db.close();
-
-  return result.records[0].get("g").properties;
-};
-
-export const getGameUsers = async (g: any) => {};
 
 // WRITE
 export const createGame = async (g: any) => {
@@ -38,17 +18,13 @@ export const createGame = async (g: any) => {
         id: randomUUID(),
         name: $name,
         owner: $owner,
-        guildId: $guildId,
         createdAt: ${Date.now()}
       })
       RETURN g`,
     {
       name: g.name,
       owner: g.owner,
-      guildId: g.guildId,
     }
   );
-  await db.close();
-
   return deserialize(result.records[0].get("g").properties);
 };
