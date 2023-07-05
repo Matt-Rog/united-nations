e8e83f3dc98e701dea28a3762bd25ee2218d10e2
Matthew Rogers <rogersmatt350@gmail.com>
2023-07-04 20:30:10 -0500

    add invite capabilities

--- api/src/router/games.ts
+++ api/src/router/games.ts
@@ -1,7 +1,23 @@
 import express from "express";
 import { isApp } from "../middlewares";
-import { initGame } from "../controllers/games";
+import {
+  deleteGame,
+  getGame,
+  initGame,
+  inviteUser,
+  resolveInvite,
+} from "../controllers/games";
+import { onBoard } from "../controllers/players";
 
 export default (router: express.Router) => {
-  router.post("/games/create", isApp, initGame);
+  router.get("/games/:gameId", isApp, getGame);
+  router.post("/games", isApp, initGame);
+  router.delete("/games/:gameId", isApp, deleteGame);
+
+  // PLAYERS
+  router.post("/games/:gameId/players", isApp, onBoard);
+
+  // INVITES
+  router.post("/games/:gameId/invites", isApp, inviteUser);
+  router.delete("/games/:gameId/invites/:inviteId", isApp, resolveInvite);
 };
