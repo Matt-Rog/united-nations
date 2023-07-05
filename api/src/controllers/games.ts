9a28114c69b35898de18eb8b3a36d51810325705
Matthew Rogers <rogersmatt350@gmail.com>
2023-07-04 20:31:59 -0500

    add invite functionality

--- api/src/controllers/games.ts
+++ api/src/controllers/games.ts
@@ -1,33 +1,64 @@
-import { createGame, getGameByProp } from "../db/models/games";
+import { createGame, getGameByProp, deleteGameById } from "../db/models/games";
 import {
   addUserToGame,
+  createUser,
   getUserByDiscordId,
   getUserById,
   getUserByProp,
+  getUserGames,
 } from "../db/models/users";
 import express from "express";
 import {
   createGameInvite,
   deleteGameInvite,
   getGameInviteByProp,
+  getGameInviteByGameAndUser,
 } from "../db/models/gameInvites";
+
+export const getGame = async (req: express.Request, res: express.Response) => {
+  try {
+    const { isDiscord } = req.query;
+    let { gameId } = req.params;
+    if (isDiscord) {
+      let game = await getGameByProp({ guildId: gameId });
+      if (!game) {
+        return res.status(400).json({ message: "Resources not found" }).end();
+      }
+      gameId = game.id;
+    }
+    let game = await getGameByProp({ id: gameId });
+    if (!game) {
+      return res.status(400).json({ message: "Resources not found" }).end();
+    }
+    return res.status(200).json(game);
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
+};
 
 // POST /games
 export const initGame = async (req: express.Request, res: express.Response) => {
   try {
     const { isDiscord } = req.query;
     let { name, guildId, ownerId } = req.body;
-
     if (isDiscord) {
-      let user = await getUserByDiscordId(ownerId);
+      let game = await getGameByProp({ guildId });
+      if (game) {
+        return res
+          .status(400)
+          .json({ message: "Game already exists for current guild." })
+          .end();
+      }
+      let user = await getUserByDiscordId(String(ownerId));
       if (!user) {
-        return res.status(400).json({ message: "Resources not found" }).end();
+        return res.status(400).json({ message: "Invalid creator" }).end();
       }
       ownerId = user.id;
     }
     let user = await getUserById(ownerId);
     if (!user) {
-      return res.status(400).json({ message: "User not found" });
+      return res.status(400).json({ message: "Invalid creator" });
     }
     const game = await createGame({ name, owner: user.id, guildId });
     const can_access = await addUserToGame(user, game);
@@ -35,6 +66,39 @@ export const initGame = async (req: express.Request, res: express.Response) => {
   } catch (error) {
     console.log(error);
     return res.sendStatus(400);
+  }
+};
+
+// DELETE /games/:gameId?userId={userId}
+export const deleteGame = async (
+  req: express.Request,
+  res: express.Response
+) => {
+  try {
+    let { gameId } = req.params;
+    const { isDiscord } = req.query;
+    let { userId } = req.query;
+    if (isDiscord) {
+      let game = await getGameByProp({ guildId: gameId });
+      let user = await getUserByDiscordId(String(userId));
+      if (!game || !user) {
+        return res.status(400).json({ message: "Resources not found" }).end();
+      }
+      gameId = game.id;
+      userId = user.id;
+    }
+    let game = await getGameByProp({ id: gameId });
+    let user = await getUserByProp({ id: userId });
+    if (!game || !user) {
+      return res.status(400).json({ message: "Resources not found" }).end();
+    }
+    if (game.owner !== user.id) {
+      return res.status(401).json({ message: "User unauthorized" }).end();
+    }
+    await deleteGameById(game.id);
+    return res.status(200).json(game).end();
+  } catch (error) {
+    return res.status(400).json({ message: error }).end();
   }
 };
 
@@ -46,23 +110,63 @@ export const inviteUser = async (
   try {
     let { gameId } = req.params;
     const { isDiscord } = req.query;
-    let { userId, expires } = req.body;
+    let { inviter, invitees } = req.body;
     if (isDiscord) {
       // All id's will be discord based
       let game = await getGameByProp({ guildId: gameId });
-      let user = await getUserByDiscordId(userId);
+      let user = await getUserByDiscordId(inviter);
       if (!game || !user) {
         return res.status(400).json({ message: "Resources not found" }).end();
       }
       gameId = game.id;
-      userId = user.id;
+      inviter = user.id;
     }
     let game = await getGameByProp({ id: gameId });
-    let user = await getUserByProp({ id: userId });
-    let invite = await createGameInvite(game, user, expires);
-    console.log(invite);
-    return res.status(200).json(invite).end();
+    inviter = await getUserByProp({ id: inviter });
+
+    // Invitier authorization check
+    let inviterGames = await getUserGames(inviter);
+    if (!inviterGames.map((game: any) => game.id).includes(game.id)) {
+      return res.status(401).json({ message: "User not authorized" }).end();
+    }
+
+    let invites = [];
+    // Create invites
+    for (var inviteRequest of invitees) {
+      // Check if invitee exists
+      let user = await getUserByProp({ id: inviteRequest.userId });
+
+      if (isDiscord) {
+        user = await getUserByDiscordId(inviteRequest.userId);
+        if (!user) {
+          // Create temp user if not existing
+          user = await createUser({
+            username: "USER YET TO CONNECT DISCORD",
+            email: inviteRequest.userId,
+            avatarUrl: "NA",
+            discord: {
+              id: inviteRequest.userId,
+              accessToken: "NA",
+            },
+          });
+        }
+      } else if (!user) {
+        return res
+          .status(400)
+          .json({ message: "Invited user does not exist." })
+          .end();
+      }
+      // Check for existing invite + cannot invite self
+      let invite = await getGameInviteByGameAndUser(game, user);
+      if (!invite && user.id !== inviter.id) {
+        invite = await createGameInvite(game, user, inviteRequest.expires);
+        invites.push(invite);
+      }
+    }
+
+    return res.status(200).json(invites).end();
   } catch (error) {
+    console.log(error);
     return res.sendStatus(400);
   }
 };
